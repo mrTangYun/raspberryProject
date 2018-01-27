219404dead6682826ebadaf2ea64416213bf8c15
唐云 <tangyun@speedto.cn>
2018-01-27 20:09:40 +0800

    用scoket控制led的例子

---
+++ nodejs/socketIo/index.js
@@ -0,0 +1,72 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var rpio = require('rpio');
+
+function on(point) {
+	// 打开 11 号针脚（GPIO17) 作为输出
+	rpio.open(point, rpio.OUTPUT);
+
+    // 指定 11 号针脚输出电流（HIGH）
+	rpio.write(point, rpio.HIGH);
+}
+
+function off(point) {
+	// 打开 11 号针脚（GPIO17) 作为输出
+	rpio.open(point, rpio.OUTPUT);
+	// 指定 11 号针脚输出电流（HIGH）
+	rpio.write(point, rpio.LOW);
+}
+
+function get(point) {
+	return rpio.read(point) ? 'high' : 'low';
+}
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/static/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('led-on', function(dataStr) {
+	  try {
+		  const data = JSON.parse(dataStr);
+		  const point = data.point;
+		  on(point || 11);
+		  socket.emit('changeStatus', JSON.stringify({
+			  errCode: 0,
+			  errText: '',
+			  point: point,
+			  status: get(point)
+		  }));
+	  } catch (e) {
+		  console.log(e);
+      }
+  });
+  socket.on('led-off', function(dataStr) {
+    try {
+	    const data = JSON.parse(dataStr);
+	    const point = data.point;
+	    off(point || 11);
+	    socket.emit('changeStatus', JSON.stringify({
+		    errCode: 0,
+		    errText: '',
+		    point: point,
+		    status: get(point)
+	    }));
+    } catch (e) {
+      console.log(e)
+    }
+  });
+
+  socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+	  io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+    
